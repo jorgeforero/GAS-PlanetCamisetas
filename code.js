70781b60c7bae3d6167cb1061ece4139c8e35adb
Jorge Forero <jorge.e.forero@gmail.com>
2020-11-30 15:06:35 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+// Planet Camisetas
+// jorge.e.forero@gmail.com
+// 2020
+
+// Definiciones
+const LABELS = [ 'timestamp', 'fullname', 'email', 'number', 'size' ];
+
+// Tallas de camisetas que se están trabajando
+const SIZES = [ 'XS', 'S', 'M', 'L', 'XL', 'XXL' ];
+
+// Url del libro
+const URLBOOK = '__XXX__';
+
